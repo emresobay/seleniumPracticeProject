c34d528547b047273d47be07fe0264f11d168481
emresobay <yemresobay@gmail.com>
2022-08-16 07:09:05 +0300

    first commit

--- src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
@@ -1,4 +1,4 @@
-package com.cydeo;
+package com.cydeo.tests.day1_selenium_intro;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
@@ -14,6 +14,7 @@ public class SeleniumTest {
         //2. Create instance of the chrome driver
         WebDriver driver = new ChromeDriver();
 
+        //3. Test if driver is working as expected
         driver.get("https://www.google.com");
     }
 }
